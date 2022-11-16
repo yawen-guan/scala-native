a926b38811e93d634c4d8c82b3c998ba390dd7cc
Yawen Guan <yawen.guan.email@gmail.com>
2022-11-16 20:56:01 +0100

    Implement `MemoryPool` and `MemoryPoolZone` in C to manage zone memory.
    In current unsafe zone, the zone memory is in the heap, managed by the
    Scala-implemented `MemoryPoolZone` in nativelib. This commit implement
    `MemoryPool` which manage a new memory area other than heap and stack, and
    `MemoryPoolZone` which implements the zone operations based on the
    memory pool. This enables the zone operations to be backend operations
    as GC operations.

---
+++ nativelib/src/main/resources/scala-native/zone/MemoryPool.c
@@ -0,0 +1,153 @@
+#include <stdlib.h>
+#include <memory.h>
+#include "MemoryPool.h"
+#include "../gc/shared/MemoryMap.h"
+
+void memorypool_alloc_chunk(MemoryPool *pool);
+void memorypool_alloc_page(MemoryPool *pool);
+
+MemoryPool *memorypool_open() {
+    MemoryPool *pool = malloc(sizeof(MemoryPool));
+    pool->chunkPageCount = MEMORYPOOL_MIN_PAGE_COUNT;
+    pool->chunk = NULL;
+    pool->page = NULL;
+    memorypool_alloc_chunk(pool);
+    return pool;
+}
+
+void memorypool_alloc_chunk(MemoryPool *pool) {
+    if (pool->chunkPageCount < MEMORYPOOL_MAX_PAGE_COUNT) {
+        pool->chunkPageCount *= 2;
+    }
+    MemoryChunk *chunk = malloc(sizeof(MemoryChunk));
+    chunk->size = MEMORYPOOL_PAGE_SIZE * pool->chunkPageCount;
+    chunk->offset = 0;
+    chunk->start = memoryMap(chunk->size);
+    chunk->next = pool->chunk;
+    pool->chunk = chunk;
+}
+
+void memorypool_alloc_page(MemoryPool *pool) {
+    if (pool->chunk->offset >= pool->chunk->size) {
+        memorypool_alloc_chunk(pool);
+    }
+    MemoryPage *page = malloc(sizeof(MemoryPage));
+    page->start = pool->chunk->start + pool->chunk->offset;
+    page->offset = 0;
+    page->next = pool->page;
+    pool->chunk->offset += MEMORYPOOL_PAGE_SIZE;
+    pool->page = page;
+}
+
+/** Borrow a single unused page, to be reclaimed later. */
+MemoryPage *memorypool_claim(void *_pool) {
+    MemoryPool *pool = (MemoryPool *)_pool;
+    if (pool->page == NULL) {
+        memorypool_alloc_page(pool);
+    }
+    MemoryPage *result = pool->page;
+    pool->page = result->next;
+    result->next = NULL;
+    result->offset = 0;
+    return result;
+}
+
+/** Reclaimed a list of previously borrowed pages. */
+void memorypool_reclaim(void *_pool, void *_head_page, void *_tail_page) {
+    MemoryPool *pool = (MemoryPool *)_pool;
+    MemoryPage *head = (MemoryPage *)_head_page;
+    MemoryPage *tail = (MemoryPage *)_tail_page;
+    tail->next = pool->page;
+    pool->page = head;
+}
+
+void memorypool_free(void *_pool) {
+    MemoryPool *pool = (MemoryPool *)_pool;
+    // Free chunks.
+    MemoryChunk *chunk = pool->chunk, *pre_chunk = NULL;
+    while (chunk != NULL) {
+        pre_chunk = chunk;
+        chunk = chunk->next;
+        memoryUnmap(pre_chunk->start, pre_chunk->size);
+        free(pre_chunk);
+    }
+    // Free pages.
+    MemoryPage *page = pool->page, *pre_page = NULL;
+    while (page != NULL) {
+        pre_page = page;
+        page = page->next;
+        free(pre_page);
+    }
+    // Free the pool.
+    free(pool);
+}
+
+// #include <stdio.h>
+
+// void debug_print_chunk(MemoryChunk *chunk, size_t idx) {
+//     printf("%02zu chunk (start: %p, size: %zx, offset: %zx)\n", idx,
+//            chunk->start, chunk->size, chunk->offset);
+// }
+
+// void debug_print_page(MemoryPage *page, size_t idx) {
+//     printf("%02zu page (start: %p, size: %x, offset: %zx)\n", idx,
+//     page->start,
+//            MEMORYPOOL_PAGE_SIZE, page->offset);
+// }
+
+// size_t debug_get_pages_length(MemoryPage *head_page, MemoryPage *tail_page) {
+//     MemoryPage *page = head_page;
+//     size_t length = 1;
+//     while (page != NULL) {
+//         if (page == tail_page)
+//             break;
+//         page = page->next;
+//         length += 1;
+//     }
+//     return length;
+// }
+
+// void debug_print_pages(MemoryPage *head_page, MemoryPage *tail_page) {
+//     printf("== pages start ==\n");
+//     MemoryPage *page = head_page;
+//     int idx = debug_get_pages_length(head_page, tail_page) - 1;
+//     while (page != NULL) {
+//         debug_print_page(page, idx);
+//         if (page == tail_page)
+//             break;
+//         page = page->next;
+//         idx -= 1;
+//     }
+//     printf("== pages end  ==\n");
+// }
+
+// size_t debug_get_chunks_length(MemoryChunk *head_chunk) {
+//     MemoryChunk *chunk = head_chunk;
+//     size_t length = 1;
+//     while (chunk != NULL) {
+//         chunk = chunk->next;
+//         if (chunk != NULL) {
+//             length += 1;
+//         }
+//     }
+//     return length;
+// }
+
+// void debug_print_chunks(MemoryChunk *head_chunk) {
+//     printf("== chunks start ==\n");
+//     MemoryChunk *chunk = head_chunk;
+//     size_t idx = debug_get_chunks_length(head_chunk) - 1;
+//     while (chunk != NULL) {
+//         debug_print_chunk(chunk, idx);
+//         chunk = chunk->next;
+//         idx -= 1;
+//     }
+//     printf("== chunks end  ==\n");
+// }
+
+// void debug_print_pool(void *_pool) {
+//     MemoryPool *pool = (MemoryPool *)_pool;
+//     printf("== pool start ==\n");
+//     debug_print_chunks(pool->chunk);
+//     printf("== pool end  ==\n");
+// }
---
+++ nativelib/src/main/resources/scala-native/zone/test.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include "MemoryPool.h"
+#include "MemoryPoolZone.h"
+
+void test_pool_claim_n_pages_then_reclaim(MemoryPool *pool, size_t n) {
+    MemoryPage *head_page = memorypool_claim(pool);
+    MemoryPage *tail_page = head_page;
+    for (int i = 0; i < n - 1; i++) {
+        MemoryPage *new_page = memorypool_claim(pool);
+        new_page->next = head_page;
+        head_page = new_page;
+    }
+    debug_print_pages(head_page, tail_page);
+    memorypool_reclaim(pool, head_page, tail_page);
+    debug_print_pool(pool);
+}
+
+void test_memorypool() {
+    MemoryPool *pool = memorypool_open();
+    test_pool_claim_n_pages_then_reclaim(pool, 10);
+    test_pool_claim_n_pages_then_reclaim(pool, 15);
+    test_pool_claim_n_pages_then_reclaim(pool, 30);
+    memorypool_free(pool);
+}
+
+void test_zone_alloc_n_byte(MemoryPoolZone *zone, size_t n) {
+    size_t size = sizeof(char) * n;
+    char *ptr = NULL; // (char *)malloc(size);
+    memorypoolzone_alloc(zone, ptr, size);
+    printf("zone alloc: size = %zx\n", size);
+    // debug_print_pages(zone->head_page, zone->head_page);
+}
+
+void test_memorypoolzone_given_pool(void *pool, int *nums, size_t nums_size) {
+    MemoryPoolZone *zone = memorypoolzone_open(pool);
+    printf("zone is open? %d\n", memorypoolzone_isopen(zone));
+    for (int i = 0; i < nums_size; i++) {
+        test_zone_alloc_n_byte(zone, nums[i]);
+    }
+    debug_print_pages(zone->head_page, zone->tail_page);
+    // memorypoolzone_free(zone); // report "Zone is still open."
+    memorypoolzone_close(zone);
+    printf("zone is open? %d\n", memorypoolzone_isopen(zone));
+    // test_zone_alloc_n_byte(zone, 0x10); // report "Zone is already closed."
+    memorypoolzone_free(zone);
+}
+
+void test_memorypoolzone() {
+    MemoryPool *pool = memorypool_open();
+
+    int nums[] = {0x10, 0x1000, 0x20, 0x700, 0x900};
+    int numsMore[] = {0x10, 0x1000, 0x20, 0x700, 0x900, 0x500, 0x300};
+
+    test_memorypoolzone_given_pool(pool, nums, 5);
+    test_memorypoolzone_given_pool(pool, nums, 5);
+    test_memorypoolzone_given_pool(pool, numsMore, 7);
+    memorypool_free(pool);
+}
+
+int main() {
+    // test_memorypool();
+    test_memorypoolzone();
+    return 0;
+}
