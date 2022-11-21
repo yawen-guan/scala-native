8aa21568549901705f1657fd587e953f9a1ef074
Yawen Guan <yawen.guan.email@gmail.com>
2022-11-21 14:01:41 +0100

    Add CMemoryPool API, use it to implement SafeZone operations.
    * CMemoryPool API provides interfaces for the C-implemented `MemoryPool`
      and `MemoryPoolZone`.
    
    * `MemoryPoolSafeZone` calls the CMemoryPool API to implement SafeZone
      operations including `open`, `close`, `isOpen` and `isClosed`.
    
    * Currently the SafeZone `alloc` operation is not implemented yet.

--- sandbox/src/main/scala/TestSafeZone.scala
+++ sandbox/src/main/scala/TestSafeZone.scala
@@ -4,10 +4,26 @@ object TestSafeZone {
   def test(): Unit = {
     println("\n<<< Test Safe Zone <<<")
 
-    SafeZone { implicit sz =>
-      val x: Int = alloc[Int]()
-      println(s"x = $x")
+    class A(var v0: Int = 0, var v1: Long = 0L) {
+      def increaseV0: Unit = v0 += 1
+      def increaseV1: Unit = v1 += 1
+      override def toString(): String = s"{$v0, $v1}"
     }
+
+    // SafeZone { implicit sz =>
+    //   val x: Int = alloc[Int]()
+    //   println(s"x = $x")
+    // }
+
+    val sz: SafeZone = SafeZone.open()
+    println(s"sz.isOpen = ${sz.isOpen}")
+    println(s"sz.isClosed = ${sz.isClosed}")
+
+    sz.close()
+
+    println(s"sz.isOpen = ${sz.isOpen}")
+    println(s"sz.isClosed = ${sz.isClosed}")
+    // sz.close() // report "Zone is already closed."
 
     println(">>> Test Safe Zone >>>\n")
   }
